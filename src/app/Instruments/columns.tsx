416870a62adde0b9af88bc35973e308b19f76885
selestino-k <sandikroon23@gmail.com>
2025-02-11 14:08:00 +0800

    first commit (nyoba nextjs dg TS + shadcn dan prisma ORM)

---
+++ src/app/Instruments/columns.tsx
@@ -0,0 +1,48 @@
+"use client"
+
+import { ColumnDef } from "@tanstack/react-table"
+
+// This type is used to define the shape of our data.
+// You can use a Zod schema here if you want.
+export type Instrumen = {
+    id_instrumen: number
+    merk_instrumen: string
+    nama_instrumen: string
+    tipe_instrumen: string
+    layanan: string
+    date: string
+    // In TypeScript, this is called a string union type.
+    // It means that the "status" property can only be one of the two strings: 'pending' or 'paid'.
+    status: 'pending' | 'operational';
+}
+
+export const columns: ColumnDef<Instrumen>[] = [
+ {
+    accessorKey: "id_instrumen",
+    header: "ID",
+  },
+  {
+    accessorKey: "merk_instrumen",
+    header: "Merk",
+  },
+  {
+    accessorKey: "nama_instrumen",
+    header: "Nama",
+  },
+  {
+    accessorKey: "tipe_instrumen",
+    header: "Tipe",
+  },
+  {
+    accessorKey: "layanan",
+    header: "Layanan",
+  },
+  {
+    accessorKey: "date",
+    header: "Tanggal",
+  },
+  {
+    accessorKey: "status",
+    header: "Status",
+  },
+]
---
+++ src/components/ui/app-sidebar.tsx
@@ -0,0 +1,92 @@
+import { Calendar, Home, Search,Calculator, FlaskConical, Armchair } from "lucide-react"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu" 
+// Menu items.
+import { Button } from "@/components/ui/button"
+
+const items = [
+  {
+    title: "Beranda",
+    url: "/",
+    icon: Home,
+  },
+  {
+    title: "Daftar Alat",
+    url: "/tabel",
+    icon: FlaskConical,
+  },
+  {
+    title: "Daftar Instrumen",
+    url: "#",
+    icon: Calculator,
+  },
+  {
+    title: "Daftar Barang",
+    url: "#",
+    icon: Armchair,
+  },
+  {
+    title: "Jadwal",
+    url: "#",
+    icon: Calendar,
+  },
+  {
+    title: "Cari",
+    url: "#",
+    icon: Search,
+  }
+  
+]
+ 
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>SI INVENTARIS LAB BIOSCIENCE</SidebarGroupLabel>
+          <SidebarGroupContent>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button variant="outline">Akun</Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-56">
+                <DropdownMenuItem>
+                Pengaturan Profil
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                Keluar
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+            </DropdownMenu>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  )
+}
