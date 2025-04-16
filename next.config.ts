caea119673d89ab9692aeec2e8848a770ac7662e
selestino-k <sandikroon23@gmail.com>
2025-04-17 03:36:55 +0800

    refactor: remove swcMinify option and enhance PrismaClient initialization with detailed logging

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  swcMinify: true,
   // If you're having issues with image domains
   images: {
     domains: [
